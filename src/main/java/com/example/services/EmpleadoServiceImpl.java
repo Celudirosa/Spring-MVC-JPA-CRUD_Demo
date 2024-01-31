b9615086b44c6b7848c28941fb4fbe57e313b632
celudirosa <cluqued@live.com>
2024-01-31 11:03:27 +0100

    add empleado service and impl

---
+++ src/main/java/com/example/services/EmpleadoServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.entities.Empleado;
+
+@Service
+public class EmpleadoServiceImpl implements EmpleadoService {
+
+    @Override
+    public List<Empleado> dameTodosLosEmpleados() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dameTodosLosEmpleados'");
+    }
+
+    @Override
+    public Empleado dameUnEmpleado(int idEmpleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dameUnEmpleado'");
+    }
+
+    @Override
+    public void eliminarEmpleado(int idEmpleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarEmpleado'");
+    }
+
+    @Override
+    public void persistirEmpleado(Empleado empleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'persistirEmpleado'");
+    }
+
+    @Override
+    public void actualizarEmpleado(Empleado empleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actualizarEmpleado'");
+    }
+
+}
