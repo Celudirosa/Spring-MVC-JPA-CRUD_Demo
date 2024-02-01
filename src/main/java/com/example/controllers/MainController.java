83785d964d136f438751166541de0e63f59244e3
celudirosa <cluqued@live.com>
2024-02-01 16:31:11 +0100

    model and view antiguo

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,18 +1,33 @@
 package com.example.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.entities.Empleado;
+import com.example.services.EmpleadoService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller // sin esto no sabe que es un controlador
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class MainController {
 
+    private final EmpleadoService empleadoService;
+
+    // antiguamente se hacia esto:
     @GetMapping("/all")
     public ModelAndView dameEmpleados() {
 
         ModelAndView modelo = new ModelAndView("views/listadoEmpleados");
+
+        List<Empleado> empleados = empleadoService.dameTodosLosEmpleados();
+
+        modelo.addObject("empleados", empleados);
 
         return modelo;
     }
